ab950bdfa4321f182c48e3558fa606ec976ef651
CMJONeill03 <>
2023-01-09 13:24:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+import uulib.Console;
+import uulib.ArrayUtils;
+
+public class Main {
+
+    // Challenge question 1
+    public static void countdown(int x) {
+        // complete this method
+
+    }
+
+    public static void question1() {
+        // COMPLETE THIS METHOD
+
+    }
+
+    // Challenge Question 2    
+    public static void question2() {
+        // Create an interactive program that calls the methods in the morse class to prove they work
+       // Morse.messageToMorse("the quick brown") and Morse.messageFromMorse("._ / ....") to prove they work
+
+
+    }
+
+
+    public static void main(String[] args) {
+        
+        //question1();
+        //question2();
+
+    }
+
+
+
+
+}
